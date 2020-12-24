84d8d02374e10638ff54531c00a2224421c797fe
Andrew Schechtman-Rook <rook166@gmail.com>
2020-12-24 14:01:09 -0500

    Abandoning footnote experiment for now

--- theme/js/custom_footnotes.js
+++ theme/js/custom_footnotes.js
@@ -1,12 +1,13 @@
-jQuery(document).ready(function() {
-    $(".footnote").each(function() {
-	// All footnote refs are the ID of the footnote prefixed by a # (an in-page reference):
-	var footnote_id = $(this).attr("href");
-	var footnote = $(footnote_id);
-	console.log(footnote_id);
-	footnote.each(function() {
-	    console.log("WOOOO");
-	    console.log($(this));
-	});
-    });
-});
+// jQuery(document).ready(function() {
+//     $(".footnote").each(function() {
+// 	// All footnote refs are the ID of the footnote prefixed by a # (an in-page reference):
+// 	var footnote_id = $(this).attr("href");
+// 	var footnote = $(footnote_id);
+// 	console.log(footnote_id);
+// 	console.log
+// 	footnote.each(function() {
+// 	    console.log("WOOOO");
+// 	    console.log($(this));
+// 	});
+//     });
+// });
