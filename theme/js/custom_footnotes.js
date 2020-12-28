b71dc134cd4055fb1f4140268e00f0beef1ec0c0
Andrew Schechtman-Rook <rook166@gmail.com>
2020-12-28 15:32:53 -0500

    trying to debug my footnotes

--- theme/js/custom_footnotes.js
+++ theme/js/custom_footnotes.js
@@ -1,29 +1,9 @@
-// jQuery(document).ready(function() {
-//     $(".footnote").each(function() {
-// 	// All footnote refs are the ID of the footnote prefixed by a # (an in-page reference):
-// 	var footnote_id = $(this).attr("href");
-// 	var footnote = $(footnote_id);
-// 	console.log(footnote_id);
-// 	console.log
-// 	footnote.each(function() {
-// 	    console.log("WOOOO");
-// 	    console.log($(this));
-// 	});
-//     });
-// });
-//
-//jQuery(document).ready(function() {
-//    $(".footnotes").css({display: "hidden");
-//});
 jQuery(document).ready(function() {
-    console.log("a");
     $(".footnotes").each(function() {
         $(this).prepend("<h2>Footnotes</h2>");
     });
     $(".content div").each(function() {
-    console.log("b");
         if ($(this).hasClass("previous-next")) {
-            console.log("Wooo");
             $(this).insertBefore($(this).prev("div"));
         }
     });
