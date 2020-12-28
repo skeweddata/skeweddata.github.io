0ac0266868c6b6f19cfbe14a63848067aaad64e5
Andrew Schechtman-Rook <rook166@gmail.com>
2020-12-28 15:35:16 -0500

    trying to debug my footnotes

--- theme/js/custom_footnotes.js
+++ theme/js/custom_footnotes.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
     $(".footnotes").each(function() {
-        $(this).prepend("<h2>Footnotes</h2>");
+        $(this).prepend("<h3>Footnotes</h3>");
     });
     $(".content div").each(function() {
         if ($(this).hasClass("previous-next")) {
